6acdb7b68c228328e962e6166e7efdcaf4edd7a4
k911 <k911@inbox.ru>
2024-08-27 17:59:33 +0300

    add search bots

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -19,6 +19,14 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="yandex-verification"
+          content={`${process.env.NEXT_PUBLIC_YANDEX_VERIFICATION}`}
+        />
+        <meta
+          name="google-site-verification"
+          content={`${process.env.NEXT_PUBLIC_GOOGLE_VERIFICATION}`}
+        />
         <link rel="canonical" href={canonicalUrl} />
       </Head>
       <Layout className={montserrat.className}>
